90e9156b5a6cab64f7fe18e75f13cc7390f8b578
Nerea López Astasio <al_nlopeza@iessierradelasvillas.org>
2022-10-25 14:39:58 +0200

    Completado ej. 2

--- Ej 2.py
+++ Ej 2.py
@@ -1,3 +1,23 @@
 import random
-numero= random.randit(1,1000)
-print(numero)
+numero= random.randint(1,100)
+escrito=0
+intentos=1
+escrito=int(input("Número: "))
+
+while intentos !=10 and escrito!=numero:
+    if escrito<numero:
+        print("El número introducido es menor que el número secreto.")
+       
+    elif escrito>numero:
+        print("El número introducido es mayor que el número secreto")
+        
+    escrito=int(input("Número: "))
+    intentos= intentos + 1
+    print("Llevas", intentos, "intentos")  
+
+
+if escrito==numero:
+    print("Has acertado")
+else:
+    print("Has llegado al límite de intentos, el número era", numero)
+
