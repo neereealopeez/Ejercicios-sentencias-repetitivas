385fc3267ce04c1f1c694099bdf1b86ae3b8d8a7
Nerea López <al_nlopeza@iessierradelasvillas.org>
2022-10-30 15:51:44 +0100

    Hecho ej. 3 y 4

--- Ej 3.py
+++ Ej 3.py
@@ -1,8 +1,11 @@
-num=0
-contador=0
-num= num=int(input("Número: "))
-while num!=0:
-    num= num=int(input("Número: "))
-    contador= +1
-if num==0:
-    print("La suma es")
+numero=0
+suma=0
+media=0
+
+numero = (int)(input("Dime un número:"))
+while numero != 0:
+    suma = suma + numero
+    media = media + 1
+    numero = (int)(input("Dime otro número:))
+
+print("La suma de todos lo números es:", suma, ", y su media es", media)
---
+++ Ej 4.py
@@ -0,0 +1,20 @@
+vNum = []
+numero= 0
+menorcont = 0
+mayorcont = 0
+igualcont=0
+cantidad = (int)("Dime la cantidad de números que vas a introducir: ")
+
+for i in range(1,cantidad):
+    numero = (int)(input("Dime un número:\n"))
+    vNum.append(numero)
+
+for i in vNum:
+    if i==0:
+        igualcont+=1
+    elif i<0:
+        menorcont+=1
+    elif i>0:
+        mayorcont+=1
+
+print("Hay:", mayorcont, "numeros mayores que 0, ", igualcont, "iguales que 0 y", menorcont, "menores de 0")º1
