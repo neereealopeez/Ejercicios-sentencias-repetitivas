28062dc89b372b92e6c89f7b5c819cf50efac5fd
Nerea López Astasio <al_nlopeza@iessierradelasvillas.org>
2022-11-03 10:15:59 +0100

    Añadidos ej. 9 y 10

---
+++ Ej.10.py
@@ -0,0 +1,8 @@
+tabla1=1
+tabla2=2
+tabla3=3
+tabla4=4
+tabla5=5
+
+for i in range (0,10):
+    tabla
---
+++ Ej.9.py
@@ -0,0 +1,18 @@
+
+from cgitb import reset
+from email.encoders import encode_base64
+
+
+base=0
+exp=0
+resultado=1
+base= (float)(input("Dime la base "))
+exp=(int)(input("Dime el exponente "))
+if exp<0:
+    print("El exponente debe ser positivo")
+    exp=(int)(input("Dime el exponente"))  
+if base !=float:
+    base=(int)(input("Dime la base"))
+for i in range(0,exp):
+     resultado=resultado*base  
+print("Potencia=", resultado)
